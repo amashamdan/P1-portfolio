526f42220a04d8ed26b4dc99f5f4229f5bb007b3
Amer Hamdan <amashamdan@gmail.com>
2016-08-17 15:30:25 -0700

    Feat: The new file contains the list of projects.
    All projects are in this file. It has an array of JSON objects on for each project.
    Each JSON has the project's section, title, page link and description.
    This file is used by scripts.js to populate the projects' sections.

---
+++ js/projects.js
@@ -0,0 +1,140 @@
+var featuredProjects = [
+	{
+		section: "featured-projects-thumbs",
+		title: "NEIGHBOURHOOD MAP",
+		link: "http://amashamdan.github.io/p5-neighbourhood-map/",
+		description: "A responsive single page app implementing MVVM archetictural pattern and AJAX to search for places of interest in a certain city. APIs used: Google maps JavaScript API, OpenWeatherMap API and New York Times API."
+	},
+	{
+		section: "featured-projects-thumbs",
+		title: "ROUGLIKE DUNGEON CRAWLER",
+		link: "http://amashamdan.github.io/rouglike/",
+		description: "Rouglike Dungeon Crawler game where the user has to find the enemy boss and defeat it. Built with React.js and Sass."
+	},
+	{
+		section: "featured-projects-thumbs",
+		title: "GAME OF LIFE",
+		link: "http://amashamdan.github.io/game-of-life/",
+		description: "Conway's Game of Life which determines the future state of a group of dead and alive cells based on their current state. Built with React.js and Sass."
+	},
+	{
+		section: "projects-thumbs",
+		title: "RECIPE BOX",
+		link: "http://amashamdan.github.io/recipe-box/",
+		description: "A responsive single page app which enables the user to view, edit, delete and add new recipes. Built with React.js and Sass."
+	},
+	{
+		section: "projects-thumbs",
+		title: "SIMON",
+		link: "http://amashamdan.github.io/simon/",
+		description: "The memory game Simon where the user has to repeat a sequence played by the CPU randomly. Built with JavaScript and jQuery."
+	}, 
+	{
+		section: "projects-thumbs",
+		title: "POMODORO CLOCK",
+		link: "http://amashamdan.github.io/pomodoro/",
+		description: "The famous pomodoro clock which helps in managing time by breaking down work into intervals: seesion and break. Built with JavaScript and jQuery."
+	},
+	{
+		section: "projects-thumbs",
+		title: "FEED READER TESTING",
+		link: "http://amashamdan.github.io/p6-feed-reader-testing/#",
+		description: "Jasmine development framework in action to test a feed reader."
+	},
+	{
+		section: "projects-thumbs",
+		title: "WEBSITE OPTIMIZATION",
+		link: "http://amashamdan.github.io/P4-website-optimization/",
+		description: "Optimized performance of a provided website to achieve a PageSpeed score of 90 for both Desktops and mobile devices. Optimized the critical rendering path to ensure a smooth scrolling experience with a consistent frame rate of 60 fps."
+	},
+	{
+		section: "projects-thumbs",
+		title: "JAVASCRIPT CALCULATOR",
+		link: "http://amashamdan.github.io/calculator/",
+		description: "A basic real life calculator which enables the user to perform and chain basic arithemtic operations. Built with JavaScript and jQuery."
+	},
+	{
+		section: "projects-thumbs",
+		title: "MARKDOWN PREVIEWER",
+		link: "http://amashamdan.github.io/markdown-previewer/",
+		description: "An application where the user can type in markdown language and see the converted text instantaneously. Built with React.js, marked libarary and Sass."
+	},
+	{
+		section: "projects-thumbs",
+		title: "TicTacToe",
+		link: "http://amashamdan.github.io/tic/",
+		description: "The Popular TicTacToe game where the player competes with the CPU. Built with JavaScript, jQuery and HTML5 canvas."
+	},
+	{
+		section: "projects-thumbs",
+		title: "FCC CAMPER LEADERBOARD",
+		link: "http://amashamdan.github.io/camper-leaderboard/",
+		description: "A page which displays the users who've earned the most Free Code Camp's points. Built with React.js and Sass."
+	},
+	{
+		section: "projects-thumbs",
+		title: "TWITCH TV USERS",
+		link: "http://amashamdan.github.io/twitch/",
+		description: "A responsive single page app which displays the status of TwichTV users. Page built using AJAX and TwitchTV API."
+	},
+	{
+		section: "projects-thumbs",
+		title: "WIKIPEDIA VIEWER",
+		link: "http://amashamdan.github.io/wikipedia-viewer/",
+		description: "An application where the user can search for a wikipedia entry or read a random article on Wikipedia. Implemented AJAX requests and wikipedia API to load the search results."
+	},
+	{
+		section: "projects-thumbs",
+		title: "LOCAL WEATHER",
+		link: "http://amashamdan.github.io/weather/",
+		description: "An application which detects the user's current location and displays its current weather. Implemented AJAX requests alongside ipinfo API to detect location and OpenWeatherMap API to fetch weather information."
+	},
+	{
+		section: "projects-thumbs",
+		title: "RANDOM QUOTE MACHINE",
+		link: "http://amashamdan.github.io/quote-machine/",
+		description: "A single page app where the user can read a randomly loaded quote and then tweet it if desired. Quotes are loaded using AJAX requests and Quoteondesign API."
+	},
+	{
+		section: "projects-thumbs",
+		title: "FROGGER",
+		link: "http://amashamdan.github.io/P3-Frogger/",
+		description: "A clone of the classic arcade game 'Frogger' developed using Object-Oriented JavaScript and HTML5 Canvas."
+	},
+	{
+		section: "projects-thumbs",
+		title: "ONLINE RESUME",
+		link: "http://amashamdan.github.io/P2-online-resume/",
+		description: "My online resume. Built using jQuery and JSON objects to populate the page."
+	},
+	{
+		section: "projects-thumbs",
+		title: "BOOTSTRAP PORTFOLIO PAGE",
+		link: "http://amashamdan.github.io/portfolio2/",
+		description: "An alternative responsive portfolio page page with Bootstrap."
+	},
+	{
+		section: "projects-thumbs",
+		title: "SAMPLE LANDING PAGE",
+		link: "https://amashamdan.github.io/sample-landing-page/",
+		description: "A sample app landing page built with Bootstrap."
+	},
+	{
+		section: "python-thumbs",
+		title: "RICE-ROCKS",
+		link: "http://www.codeskulptor.org/#user40_Cvu4E4CENS_0.py",
+		description: "A clone of the classic arcade game Asteroids developed using object oriented programming in Python. Program templateand artwork were provided."
+	},
+	{
+		section: "python-thumbs",
+		title: "BLACKJACK",
+		link: "http://www.codeskulptor.org/#user40_UNgu0do2XVlHqzD.py",
+		description: "A simplified version of the card game Blackjack DEVELOPED using event driven programming in Python. Program template was provided."
+	},
+	{
+		section: "python-thumbs",
+		title: "PONG",
+		link: "http://www.codeskulptor.org/#user40_0EqZ4HslXO_0.py",
+		description: "A clone of the classic two-players game 'Pong' developed using event driven programming in Python. Program template was provided. Program template was provided."
+	},
+];
