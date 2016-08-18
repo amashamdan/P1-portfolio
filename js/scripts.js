bf92a70259b732d1f49363d5043432429e52a971
Amer Hamdan <amashamdan@gmail.com>
2016-08-18 13:08:57 -0700

    Feat: Restored functionality of showing description when a project's photo is hovered.
    When the project div is hovered, the description fades in above the project's photo.
    Now the description's window width is the same as the photo width... it was the same as the row width before.

--- js/scripts.js
+++ js/scripts.js
@@ -4,45 +4,21 @@ for (var project in featuredProjects) {
 	smallTitle = title.toLowerCase();
 	smallTitle = smallTitle.replace(/ /g, "-");
 	var link = featuredProjects[project].link; 
-	$(section).append('<div class="col-3-center-align"><img class="project-photo" id="'+ smallTitle +'><" src="images/'+ smallTitle +'.jpg" alt="A thumbnail of project"><h3 class="project-title">'+ title +'</h3><a class="project-link" target="blank" href="'+link+'">Project link</a></div>');
+	var description = featuredProjects[project].description;
+	$(section).append('<div class="col-3-center-align project" id="'+ smallTitle +'"><img class="project-photo"  src="images/'+ smallTitle +'.jpg" alt="A thumbnail of project"><h3 class="project-title">'+ title +'</h3><a class="project-link" target="blank" href="'+link+'">Project link</a><p class="project-details">'+ description +'</p></div>');
 }
 
-/*Script to show details about a project once its photo is hovered
-$("#online-resume").hover(function(){
-	var top = (($("#online-resume").position()).top) + $("#online-resume").height() + 55;
-	$(".online-resume-details").css({ "top": top+"px"});
-	$(".online-resume-details").fadeToggle();
+//Script to show details about a project once its photo is hovered
+$(".project").hover(function(){
+	var width = $(this).find(".project-photo").width();
+	var top = $(this).find(".project-photo").position().top;
+	var left = $(this).find(".project-photo").position().left;
+	$(this).find(".project-details").css({"width": width, "top": top + 7, "left": left + 5});
+	$(this).find(".project-details").fadeToggle("swing");
 });
 
-$("#arcade-game").hover(function(){
-	var top = (($("#arcade-game").position()).top) + $("#arcade-game").height() + 55;
-	$('.arcade-game-details').css({ 'top': top+"px"});
-	$(".arcade-game-details").fadeToggle();
-});
 
-$("#website-optimization").hover(function(){
-	var top = (($("#website-optimization").position()).top) + $("#website-optimization").height() + 55;
-	$(".website-optimization-details").css({ "top": top+"px"});
-	$(".website-optimization-details").fadeToggle();
-});
 
-$("#neighborhood-map").hover(function(){
-	var top = (($("#neighborhood-map").position()).top) + $("#neighborhood-map").height() + 55;
-	$(".neighborhood-map-details").css({ "top": top+"px"});
-	$(".neighborhood-map-details").fadeToggle();
-});
-
-$("#feed-reader").hover(function(){
-	var top = (($("#feed-reader").position()).top) + $("#feed-reader").height() + 55;
-	$(".feed-reader-details").css({ "top": top+"px"});
-	$(".feed-reader-details").fadeToggle();
-});
-
-$("#project6").hover(function(){
-	var top = (($("#project6").position()).top) + $("#project6").height() + 55;
-	$(".project6-details").css({ "top": top+"px"});
-	$(".project6-details").fadeToggle();
-});*/
 /*script for the functionality of the drawer
 var menu = document.querySelector(".menu");
 var header = document.querySelector(".header");
